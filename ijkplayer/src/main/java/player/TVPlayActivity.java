aaf3752a9068de59d4b44ba83f43a0e4392d7022
allen du <allen@allendeMac-mini.local>
2018-06-22 16:16:34 +0800

    调整为横屏

--- ijkplayer/src/main/java/player/TVPlayActivity.java
+++ ijkplayer/src/main/java/player/TVPlayActivity.java
@@ -6,14 +6,18 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.afap.ijkplayer.R;
 import com.shuyu.gsyvideoplayer.GSYVideoManager;
 import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
 import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
 
-public class TVPlayActivity extends Activity {
+public class TVPlayActivity extends AppCompatActivity {
 
     StandardGSYVideoPlayer videoPlayer;
     private String mVideoPath;
@@ -29,8 +33,25 @@ public class TVPlayActivity extends Activity {
     }
 
     @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        GSYVideoManager.releaseAllVideos();
+        mVideoPath = getIntent().getStringExtra("videoPath");
+        mVideoTitle = getIntent().getStringExtra("videoTitle");
+        Log.e("tag", "onNewINtent执行了 地址："+mVideoPath);
+        init();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //去除标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //去除状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         mVideoPath = getIntent().getStringExtra("videoPath");
         mVideoTitle = getIntent().getStringExtra("videoTitle");
         setContentView(R.layout.activity_tvplay);
@@ -41,7 +62,8 @@ public class TVPlayActivity extends Activity {
         videoPlayer =  (StandardGSYVideoPlayer)findViewById(R.id.video_player);
 
         String source1 = mVideoPath;
-        videoPlayer.setUp(source1, true, mVideoTitle);
+
+        videoPlayer.setUp(source1, false, mVideoTitle);
 
 
         //增加title
@@ -50,13 +72,14 @@ public class TVPlayActivity extends Activity {
         videoPlayer.getBackButton().setVisibility(View.VISIBLE);
         //设置旋转
         orientationUtils = new OrientationUtils(this, videoPlayer);
-        //设置全屏按键功能,这是使用的是选择屏幕，而不是全屏
-        videoPlayer.getFullscreenButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                orientationUtils.resolveByClick();
-            }
-        });
+
+//        //设置全屏按键功能,这是使用的是选择屏幕，而不是全屏
+//        videoPlayer.getFullscreenButton().setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                orientationUtils.resolveByClick();
+//            }
+//        });
         //是否可以滑动调整
         videoPlayer.setIsTouchWiget(true);
         //设置返回按键功能
@@ -80,6 +103,7 @@ public class TVPlayActivity extends Activity {
     protected void onResume() {
         super.onResume();
         videoPlayer.onVideoResume();
+        Log.e("tag", "重新开始");
     }
 
     @Override
@@ -92,11 +116,11 @@ public class TVPlayActivity extends Activity {
 
     @Override
     public void onBackPressed() {
-        //先返回正常状态
-        if (orientationUtils.getScreenType() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
-            videoPlayer.getFullscreenButton().performClick();
-            return;
-        }
+//        //先返回正常状态
+//        if (orientationUtils.getScreenType() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+//            videoPlayer.getFullscreenButton().performClick();
+//            return;
+//        }
         //释放所有
         videoPlayer.setVideoAllCallBack(null);
         super.onBackPressed();
